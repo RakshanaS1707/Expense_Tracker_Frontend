30e7349dae63d0aa7d37854ad5b4ab734e378203
Rakshana <rakshblaze17@gmail.com>
2025-07-10 17:27:45 +0530

    final change

--- src/api_service/api.js
+++ src/api_service/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Set up Axios instance
-export const BASE_URL = 'http://127.0.0.1:8000/api';
-// export const BASE_URL = "https://expense-tracker-backend-q61k.onrender.com/api"
+// export const BASE_URL = 'http://127.0.0.1:8000/api';
+export const BASE_URL = "https://expense-tracker-backend-q61k.onrender.com/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
